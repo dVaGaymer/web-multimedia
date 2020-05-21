b6161b7936119fadfe3bdb90c9886c835a01e4ea
BuildTools <unconfigured@null.spigotmc.org>
2020-05-21 18:00:04 +0200

    nosotros

--- js/menu.js
+++ js/menu.js
@@ -4,3 +4,18 @@ btnMenu.addEventListener('click',function(){
    'use strict';
     menu.classList.toggle('mostrar');
 });
+
+function copyToClipBoard(){
+    /* Get the text field */
+  var copyText = "999-999-999";
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
